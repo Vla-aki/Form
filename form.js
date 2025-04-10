54db76f7b449aed5f74c4972a2dda2b962c37f02
Vla-aki <roland.csak56@gmail.com>
2025-04-10 10:08:09 +0200

    form.js

---
+++ form.js
@@ -0,0 +1,20 @@
+// Űrlap elérése, eseményfigyelő hozzáadása
+document.getElementById('userForm').addEventListener('submit', (e) => {
+    //HTML űrlap alapértelmezett viselkedés megakadályozása
+    e.preventDefault();
+    //FormData objektum létrehozása, az űrlap esemény hozzárendelése
+    //(e.target) az esemény címzettje
+    const myformData = new FormData(e.target);
+    //Az űrlap adatok áthelyezése a userData objektumba
+    const userData = Object.fromEntries(myformData);
+    
+    console.log(userData)
+    //Másolat készítése a(z) userData objektumról
+    const userObjekt = {
+        Vezetéknév: userData.vezeteknev,
+        Keresztnév: userData.keresztnev,
+        Email: userData.email,
+        Telefon: userData.telefonszam
+    }
+    console.log(userData)
+})
